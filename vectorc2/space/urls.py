eae43f9ec3838bf9da3eb326641fa0cc81a26213
Kruk, Sebastian <Sebastian.Kruk@dynatrace.com>
2019-02-15 13:57:24 +0100

    #1 cleanup from examples

--- vectorc2/space/urls.py
+++ vectorc2/space/urls.py
@@ -5,6 +5,4 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<space_name>[^/]+)/$', views.room, name='room'),
 ]
--- vectorc2/space/views.py
+++ vectorc2/space/views.py
@@ -3,10 +3,3 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 
-def index(request):
-    return render(request, 'space/index.html', {})
-
-def room(request, space_name):
-    return render(request, 'space/room.html', {
-        'space_name_json': mark_safe(json.dumps(space_name))
-})
