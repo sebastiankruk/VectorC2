6ed4de4cb62e7a32ee7ebd1045cccde137b864e6
Kruk, Sebastian <Sebastian.Kruk@dynatrace.com>
2019-02-16 16:53:44 +0100

    #3 added connectors to block

--- vectorc2/webview/static/script/blockly/blocks/vector_say_text.js
+++ vectorc2/webview/static/script/blockly/blocks/vector_say_text.js
@@ -12,6 +12,8 @@ Blockly.Blocks['vector_say_text'] = {
         .appendField("at relative speed")
         .appendField(new Blockly.FieldNumber(1, 0.1, 2, 0.1), "duration_scalar");
     this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour(150);
  this.setTooltip("Vector will say something");
  this.setHelpUrl("https://developer.anki.com/vector/docs/generated/anki_vector.robot.html#anki_vector.robot.Robot.say_text");
