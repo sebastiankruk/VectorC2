4ef5133314971d201261633ff1157dc54f8caba1
Kruk, Sebastian <Sebastian.Kruk@dynatrace.com>
2019-02-18 22:25:48 +0100

    #10 drive on/off charger, drive_straight, speed and distance utils

--- vectorc2/webview/static/script/blockly/generators/python/vector_say_text.js
+++ vectorc2/webview/static/script/blockly/generators/python/vector_say_text.js
@@ -3,7 +3,6 @@ Blockly.Python['vector_say_text'] = function(block) {
   var value_text = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
   var checkbox_use_vector_voice = (block.getFieldValue('use_vector_voice') == 'TRUE') ? 'True' : 'False';
   var number_duration_scalar = block.getFieldValue('duration_scalar');
-  // TODO: Assemble Python into code variable.
 
   var code = `${variable_robot_var}.say_text(${value_text}, ${checkbox_use_vector_voice}, ${number_duration_scalar})`
   return code;
