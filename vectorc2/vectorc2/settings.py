7ed9a28227132e3c12b8d3b01bef64ec954a078f
Kruk, Sebastian <Sebastian.Kruk@dynatrace.com>
2019-02-24 18:33:40 +0100

    #6 load bootstrap files internally

--- vectorc2/vectorc2/settings.py
+++ vectorc2/vectorc2/settings.py
@@ -147,14 +147,14 @@ BOOTSTRAP4 = {
     # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
     # or a dict like the default value below.
     "css_url": {
-        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
+        "href": "/static/style/bootstrap/bootstrap.min.css",
         "integrity": "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
         "crossorigin": "anonymous",
     },
 
     # The complete URL to the Bootstrap JavaScript file
     "javascript_url": {
-        "url": "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js",
+        "url": "/static/script/bootstrap/bootstrap.min.js",
         "integrity": "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T",
         "crossorigin": "anonymous",
     },
@@ -164,21 +164,21 @@ BOOTSTRAP4 = {
 
     # The URL to the jQuery JavaScript file (full)
     "jquery_url": {
-        "url": "https://code.jquery.com/jquery-3.3.1.min.js",
+        "url": "/static/script/bootstrap/jquery-3.3.1.min.js",
         "integrity": "sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT",
         "crossorigin": "anonymous",
     },
 
     # The URL to the jQuery JavaScript file (slim)
     "jquery_slim_url": {
-        "url": "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+        "url": "/static/script/bootstrap/jquery-3.3.1.slim.min.js",
         "integrity": "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
         "crossorigin": "anonymous",
     },
 
     # The URL to the Popper.js JavaScript file (slim)
     "popper_url": {
-        "url": "https://unpkg.com/popper.js/dist/umd/popper.min.js",
+        "url": "/static/script/bootstrap/popper.min.js",
         # "integrity": "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49",
         "crossorigin": "anonymous",
     },
