51f6e60a9197f031fd9bf43a8b798c41f9a64c61
Sebastian Kruk <dev@kruk.me>
2019-04-29 19:36:53 +0200

    #23: refactoring before mutating vector_say_text

--- vectorc2/webview/static/script/vector/generators/python/vector_say_text.js
+++ vectorc2/webview/static/script/vector/generators/python/vector_say_text.js
@@ -15,19 +15,24 @@
  * 
  * @author vectorc2@kruk.me
  */
-Blockly.Python['vector_say_text_simple'] = function(block) {
-  var variable_robot_var = Blockly.Python.variableDB_.getName(block.getFieldValue('robot_var'), Blockly.Variables.NAME_TYPE);
-  var value_text = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
+/* jshint esversion: 6 */
+Blockly.Python.vector_say_text_simple = function(block) {
+  let variable_robot_var = Blockly.Python.variableDB_
+                                  .getName(VectorUtils.getFieldValue(block, 'robot_var', 'robot'), 
+                                           Blockly.Variables.NAME_TYPE);
+  let value_text = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
 
-  var code = `${variable_robot_var}.say_text(${value_text})\n`
+  let code = `${variable_robot_var}.say_text(${value_text})\n`;
   return code;
 };
-Blockly.Python['vector_say_text'] = function(block) {
-  var variable_robot_var = Blockly.Python.variableDB_.getName(block.getFieldValue('robot_var'), Blockly.Variables.NAME_TYPE);
-  var value_text = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
-  var checkbox_use_vector_voice = GeneratorUtils.getPythonBooleanValue(block, 'use_vector_voice');
-  var number_duration_scalar = block.getFieldValue('duration_scalar');
+Blockly.Python.vector_say_text = function(block) {
+  let variable_robot_var = Blockly.Python.variableDB_
+                                  .getName(block.getFieldValue('robot_var'), 
+                                           Blockly.Variables.NAME_TYPE);
+  let value_text = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
+  let checkbox_use_vector_voice = GeneratorUtils.getPythonBooleanValue(block, 'use_vector_voice');
+  let number_duration_scalar = block.getFieldValue('duration_scalar');
 
-  var code = `${variable_robot_var}.say_text(${value_text}, ${checkbox_use_vector_voice}, ${number_duration_scalar})\n`
+  let code = `${variable_robot_var}.say_text(${value_text}, ${checkbox_use_vector_voice}, ${number_duration_scalar})\n`;
   return code;
 };
