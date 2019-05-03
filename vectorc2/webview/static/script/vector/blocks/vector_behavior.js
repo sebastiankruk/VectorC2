19851b48d1c0dd520f039292e1fe47f273cb25d5
Kruk, Sebastian <dev@kruk.me>
2019-05-03 22:20:27 +0200

    #23: drive straight ready

--- vectorc2/webview/static/script/vector/blocks/vector_behavior.js
+++ vectorc2/webview/static/script/vector/blocks/vector_behavior.js
@@ -23,7 +23,53 @@
   VectorMutator.init('vector_behavior_drive_on_charger_ex');
   VectorMutator.init('vector_dock_with_cube_ex');
   VectorMutator.init('vector_set_eye_color_ex');
-  VectorMutator.init('vector_behavior_drive_straight_ex'); //TODO make speed, animation, repeat
+  VectorMutator.init('vector_behavior_drive_straight_ex', {
+/*
+    {
+      "type": "input_value",
+      "name": "speed",
+      "check": "Speed",
+      "align": "RIGHT"
+    },
+*/
+    speed: {
+      blockCreateFunction: (context) =>  context.appendValueInput("SPEED_VAR")
+                                                .setCheck("Speed")
+                                                .setAlign(Blockly.ALIGN_RIGHT)
+                                                .appendField("at speed")     
+    },
+/*    {
+      "type": "field_checkbox",
+      "name": "should_play_anim",
+      "checked": true
+    },
+*/
+    should_play_anim: {
+      align: Blockly.ALIGN_RIGHT,
+      isPreLabel: false,
+      isPostLabel: true,
+      blockFieldFunction: () => new Blockly.FieldCheckbox("TRUE")
+    },
+/*
+    {
+      "type": "input_dummy",
+      "align": "RIGHT"
+    },
+    {
+      "type": "field_number",
+      "name": "num_retries",
+      "value": 0,
+      "min": 0,
+      "max": 10,
+      "precision": 1
+    }
+*/
+    num_retries: {
+      align: Blockly.ALIGN_RIGHT,
+      isPostLabel: true,
+      blockFieldFunction: () => new Blockly.FieldNumber(0, 0, 10, 1)
+    }
+}); //TODO make speed, animation, repeat
   VectorMutator.init('vector_behavior_turn_in_place_ex'); //TODO make speed, acceleration, complete toleration, absolute, repeat
   VectorMutator.init('vector_set_eye_color_hue_saturation_ex'); 
   VectorMutator.init('vector_set_head_angle_ex'); 
