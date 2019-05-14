f1be93d1cf15e0e99b5df88093684bac19668c6c
Sebastian Kruk <dev@kruk.me>
2019-05-14 23:12:13 +0200

    #30: migrations to create DB entries based on interactions with Vector

--- vectorc2/blocks/models.py
+++ vectorc2/blocks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class AnimationName(models.Model):
+  """
+  This model will store all animation names available for Vector
+  """
+  name = models.CharField('Animation name', max_length=50, unique=True, null=False, db_index=True)
+  active = models.BooleanField('Is active', default=True, null=False)
+
+
+class AnimationTrigger(models.Model):
+  """
+  This model will store all animation trigger names available for Vector
+  """
+  name = models.CharField('Animation trigger', max_length=50, unique=True, null=False, db_index=True)
+  active = models.BooleanField('Is active', default=True, null=False)
+
