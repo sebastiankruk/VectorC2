436f9a34f09668b5b5d2a6c53dc041b62216bf34
Sebastian Kruk <dev@kruk.me>
2019-05-19 07:25:43 +0200

    #30 implementing generator functions

---
+++ vectorc2/blocks/static/script/vector/generators/javascript/vector_animations.js
@@ -0,0 +1,32 @@
+/**
+ *  Copyright 2019 Sebastian Ryszard Kruk <vectorc2@kruk.me>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ * @author vectorc2@kruk.me
+ */
+/* jshint esversion: 6 */
+/**
+ * Generator of the JavaScript code for the first block: vector_say_text
+ */
+Blockly.JavaScript.play_animation = function(block) {
+  let variable_robot_var = VectorUtils.getRobotVar(block);
+
+  let value_text = Blockly.JavaScript.valueToCode(block, 'animation', Blockly.JavaScript.ORDER_ATOMIC);
+  // let param_speed = VectorUtils.getNumberFieldAsParam(block, 'SPEED_VAR');
+  // let param_voice = VectorUtils.getBoolFieldAsParam(block, 'VOICE_VAR',  (param_speed !== '') ? 'TRUE' : null);
+
+  let code = `${variable_robot_var}.playAnimation(${value_text});\n`;
+
+  return code;
+};
