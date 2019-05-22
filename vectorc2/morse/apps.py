29b14c84a2d8ca2947b37bf2be7fce963dd7a23d
Sebastian Kruk <dev@kruk.me>
2019-05-22 22:35:14 +0200

    #34 step by step towards morse code support

---
+++ vectorc2/morse/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MorseConfig(AppConfig):
+    name = 'morse'
---
+++ vectorc2/morse/code.py
@@ -0,0 +1,31 @@
+#  Copyright 2019 Sebastian Ryszard Kruk <vectorc2@kruk.me>
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.' 
+        }
+
+CODE_REVERSED = {value:key for key,value in CODE.items()}
