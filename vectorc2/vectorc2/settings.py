7d002b15fd7bb7d7575c397eeaccd668ab633072
Sebastian Kruk <dev@kruk.me>
2019-05-23 22:36:44 +0200

    #34 photos app sceleton

--- vectorc2/vectorc2/settings.py
+++ vectorc2/vectorc2/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'octicons',
     'nonicons',
     'blocks',
+    'photos',
+    'morse',
     'webview.apps.WebviewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
