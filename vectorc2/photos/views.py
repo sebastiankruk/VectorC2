41a8744a53dfc7a00dc1416dffe13f610e326b4a
Sebastian Kruk <dev@kruk.me>
2019-06-04 23:51:44 +0200

    #34 sending proper communication back on errors or not

--- vectorc2/photos/views.py
+++ vectorc2/photos/views.py
@@ -1,19 +1,38 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from django.utils.translation import gettext as _
+
 from photos.forms import UploadFileForm
 
 def upload(request):
   """
   This view will handle uploading of the new photos to the collection
+  More info at: https://docs.djangoproject.com/en/2.2/topics/http/file-uploads/
   """
   if request.method == 'POST':
     form = UploadFileForm(data=request.POST, files=request.FILES)
     if form.is_valid():
       form.save()
-      print('valid form')
+      response = {
+        'status': 200,
+        'content': json.dumps({
+          'id': form.instance.id,
+          'image': form.instance.image.name,
+          'label': form.instance.label
+        }),
+        "content_type": "application/json"
+      }
     else:
-      print('invalid form')
-      print(form.errors)
+      response = {
+        'status': 400,
+        'content': str(form.errors)
+      }
+  else:
+    response = {
+      'status': 405,
+      'content': _('Only POST is allowed')
+    }
 
-  return HttpResponse('Done')
-  # return HttpResponseRedirect('/photos/uploaded') 
+  return HttpResponse(**response)
