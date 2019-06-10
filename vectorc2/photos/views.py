20e22cbe0cd66c2b23720a681a36f2f7dc15432a
Sebastian Kruk <dev@kruk.me>
2019-06-10 22:57:53 +0200

    #34 photos/list API

--- vectorc2/photos/views.py
+++ vectorc2/photos/views.py
@@ -2,9 +2,13 @@ import json
 
 from django.http import HttpResponseRedirect, HttpResponse
 from django.utils.translation import gettext as _
+from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from photos.forms import UploadFileForm
 from photos.models import UserPhotos
+
+
 
 def upload(request):
   """
@@ -37,12 +41,53 @@ def upload(request):
 
   return HttpResponse(**response)
 
-def list(request):
-    return render(request, 
-                  'webview/index.html',
-                   {
-                    'photos':  UserPhotos.objects.all() 
-                  })
+
+
+def list_photos(request):
+  """
+  View function that enables to access photos.j
+  There are two parameters expected:
+  - max_count - maximum number of photos to return
+  - offset - which is the first photo index to return
+
+  This call will return the following JSON object:
+  {
+    'content': {
+      'offset': $offset_requested,
+      'max_count': $max_count,
+      'count': $actual_count_of_photos_in_the_set,
+      'photos': 'HTML mixim'
+    }
+    'status': $http_status_code
+  } 
+  """
+  if request.method == 'GET':
+    offset = int(request.GET.get('offset', 0))
+    max_count = int(request.GET.get('max_count', -1))
+
+    qs_photos = UserPhotos.objects.order_by('label', 'upload_date')
+    photos = qs_photos[offset:max_count] if (max_count > 0) else qs_photos[offset:]
+    photos_mixim = render_to_string('photos/photos_in_gallery.html', { 'photos': photos }, request)
+
+    response = {
+      'content': json.dumps({
+        'offset': offset,
+        'max_count': max_count,
+        'count': qs_photos.count(),
+        'photos': photos_mixim,
+      }),
+      'content_type': 'application/json',
+      'status': 200
+    }
+  else:
+    response = {
+      'content': _('Only POST is allowed'),
+      'status': 405
+    }
+
+  return HttpResponse(**response)
+  
+
 
 def delete(request):
   pass
