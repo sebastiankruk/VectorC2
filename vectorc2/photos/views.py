2e5ea2e5a77dca322ebf3b7ed2a3e66140f23fe1
Kruk, Sebastian <dev@kruk.me>
2019-06-14 21:52:37 +0200

    #34 photos rest api clean up

--- vectorc2/photos/views.py
+++ vectorc2/photos/views.py
@@ -9,42 +9,61 @@ from photos.forms import UploadFileForm
 from photos.models import UserPhotos
 
 
-
-def upload(request):
+def photo(request, id=None):
   """
-  This view will handle uploading of the new photos to the collection
-  More info at: https://docs.djangoproject.com/en/2.2/topics/http/file-uploads/
+  Enables to retrieve html mixin for or delete a single photo id
   """
-  if request.method == 'POST':
-    form = UploadFileForm(data=request.POST, files=request.FILES)
-    if form.is_valid():
-      form.save()
-      response = {
-        'status': 200,
-        'content': json.dumps({
-          'id': form.instance.id,
-          'image': form.instance.image.name,
-          'label': form.instance.label,
-          'html': render_to_string('photos/photos_in_gallery.html', { 'photos': [ form.instance ] }, request)
-        }),
-        "content_type": "application/json"
-      }
+  if id is None:
+    if request.method == 'GET': # /list
+      response = _list_photos(request)
+    elif request.method == 'POST': # /upload
+      response = _upload_single_photo(request)
     else:
       response = {
-        'status': 400,
-        'content': str(form.errors)
+        'content': _('Only GET or POST are allowed when ID is NOT provided'),
+        'status': 405
       }
   else:
-    response = {
-      'status': 405,
-      'content': _('Only POST is allowed')
-    }
+    if request.method == 'GET':
+      response = _get_single_photo(request, id)
+    elif request.method == 'DELETE':
+      response = _remove_single_photo(request, id)
+    else:
+      response = {
+        'content': _('Only GET or DELETE are allowed when ID is provided'),
+        'status': 405
+      }
 
   return HttpResponse(**response)
 
 
+def _upload_single_photo(request):
+  '''
+  Enables to upload photo coming in the POST payload
+  '''
+  form = UploadFileForm(data=request.POST, files=request.FILES)
+  if form.is_valid():
+    form.save()
+    response = {
+      'status': 200,
+      'content': json.dumps({
+        'id': form.instance.id,
+        'image': form.instance.image.name,
+        'label': form.instance.label,
+        'html': render_to_string('photos/photos_in_gallery.html', { 'photos': [ form.instance ] }, request)
+      }),
+      "content_type": "application/json"
+    }
+  else:
+    response = {
+      'status': 400,
+      'content': str(form.errors)
+    }
 
-def list_photos(request):
+  return response
+
+
+def _list_photos(request):
   """
   View function that enables to access photos.j
   There are two parameters expected:
@@ -62,63 +81,51 @@ def list_photos(request):
     'status': $http_status_code
   } 
   """
-  if request.method == 'GET':
-    offset = int(request.GET.get('offset', 0))
-    max_count = int(request.GET.get('max_count', -1))
+  offset = int(request.GET.get('offset', 0))
+  max_count = int(request.GET.get('max_count', -1))
 
-    qs_photos = UserPhotos.objects.order_by('label', 'upload_date')
-    photos = qs_photos[offset:max_count] if (max_count > 0) else qs_photos[offset:]
-    photos_mixim = render_to_string('photos/photos_in_gallery.html', { 'photos': photos }, request)
+  qs_photos = UserPhotos.objects.order_by('label', 'upload_date')
+  photos = qs_photos[offset:max_count] if (max_count > 0) else qs_photos[offset:]
+  photos_mixim = render_to_string('photos/photos_in_gallery.html', { 'photos': photos }, request)
 
-    response = {
-      'content': json.dumps({
-        'offset': offset,
-        'max_count': max_count,
-        'count': qs_photos.count(),
-        'html': photos_mixim,
-      }),
-      'content_type': 'application/json',
-      'status': 200
-    }
-  else:
-    response = {
-      'content': _('Only POST is allowed'),
-      'status': 405
-    }
-
-  return HttpResponse(**response)
+  return {
+    'content': json.dumps({
+      'offset': offset,
+      'max_count': max_count,
+      'count': qs_photos.count(),
+      'html': photos_mixim,
+    }),
+    'content_type': 'application/json',
+    'status': 200
+  }
   
 
+def _get_single_photo(request, id):
+  '''
+  Returns information about a single photon in a form ready to be sent back
+  '''
+  photo = UserPhotos.objects.filter(id=id)
+  photos_mixim = render_to_string('photos/photos_in_gallery.html', { 'photos': [ photo ] }, request)
 
-def photo(request, id=None):
-  """
-  Enables to retrieve html mixin for or delete a single photo id
-  """
-  if id is not None and request.method == 'GET':
-    photo = UserPhotos.objects.filter(id=id)
-    photos_mixim = render_to_string('photos/photos_in_gallery.html', { 'photos': [ photo ] }, request)
+  return {
+    'content': json.dumps({
+      'count': photo.count(),
+      'html': photos_mixim,
+    }),
+    'content_type': 'application/json',
+    'status': 200
+  }
 
-    response = {
-      'content': json.dumps({
-        'count': photo.count(),
-        'html': photos_mixim,
-      }),
-      'content_type': 'application/json',
-      'status': 200
-    }
-  elif id is not None and request.method == 'DELETE':
-    photo = UserPhotos.objects.filter(id=id).delete()
-    response = {
-      'content': json.dumps({
-        
-      }),
-      'content_type': 'application/json',
-      'status': 200
-    }
-  else:
-    response = {
-      'content': _('Only GET or DELETE is allowed'),
-      'status': 405
-    }
 
-  return HttpResponse(**response)
+def _remove_single_photo(request, id):
+  '''
+  Removes photo with given id
+  '''
+  photo = UserPhotos.objects.filter(id=id).delete()
+  return {
+    'content': json.dumps({
+      
+    }),
+    'content_type': 'application/json',
+    'status': 200
+  }
