89171d49b6930c06ba3d3c9745d2f5da1b7b5803
Kruk, Sebastian <dev@kruk.me>
2019-06-25 19:15:12 +0200

    #34 preparing to implement vector_photos generators

---
+++ vectorc2/blocks/static/script/vector/generators/javascript/vector_photo.js
@@ -0,0 +1,32 @@
+/**
+ *  Copyright 2019 Sebastian Ryszard Kruk <vectorc2@kruk.me>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ * @author vectorc2@kruk.me
+ */
+Blockly.JavaScript['vector_set_screen_image'] = function(block) {
+  var value_image_ref = Blockly.JavaScript.valueToCode(block, 'image_ref', Blockly.JavaScript.ORDER_ATOMIC);
+  var number_duration_sec = block.getFieldValue('duration_sec');
+  var checkbox_interrupt_running = block.getFieldValue('interrupt_running') == 'TRUE';
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['vector_select_photo'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
