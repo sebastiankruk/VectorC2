925d0eed20a126b2207574931f4f7b04e9cb7fb2
Kruk, Sebastian <dev@kruk.me>
2019-07-05 23:09:29 +0200

    #15 fixes

--- vectorc2/photos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from images import views
-
-urlpatterns = [
-    path('', views.image, name='REST API for images'),
-    path('<int:id>', views.image, name='REST API for images'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
